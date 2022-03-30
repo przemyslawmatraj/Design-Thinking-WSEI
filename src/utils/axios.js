1c7fc065ebcc77ab4d2faa6fad219bdbcccd0f34
Przemyslaw_Matraj <przem.matt@gmail.com>
2022-03-30 14:00:58 +0200

    Change headers in post request

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,8 +2,4 @@ import axios from 'axios';
 
 export default axios.create({
   baseURL: `http://localhost:8080`,
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-  },
 });
--- src/views/RegisterPage/index.js
+++ src/views/RegisterPage/index.js
@@ -125,6 +125,8 @@ const RegisterPage = () => {
           headers: {
             'Content-Type': 'application/json',
             'Access-Control-Allow-Credentials': true,
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
           },
           withCredentials: true,
         }
