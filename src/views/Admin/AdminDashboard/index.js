95b8b5473a2d94407e9f1ca155564273b36c14d6
przemyslawmatraj <Przemyslaw.Matraj@microsoft.wsei.edu.pl>
2022-05-27 13:00:31 +0200

    Remove embeded facebook test post

--- src/views/Admin/AdminDashboard/index.js
+++ src/views/Admin/AdminDashboard/index.js
@@ -46,17 +46,6 @@ const AdminDashboard = () => {
           <Spinner />
         </Card>
         <Announcement />
-        <iframe
-          title="post"
-          src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fradiozet%2Fposts%2F10160073370089530&show_text=true&width=500"
-          width="500"
-          height="513"
-          style={{ border: 'none', overflow: 'hidden' }}
-          scrolling="no"
-          frameBorder="0"
-          allowFullScreen="true"
-          allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
-        ></iframe>
       </div>
     </Container>
   )
